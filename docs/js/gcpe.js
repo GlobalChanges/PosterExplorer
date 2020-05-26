4ca6c10171dd000c613412aba454f9510fa9b134
Unknown <michael.kahle@yahoo.de>
2020-05-26 14:06:20 +0200

    Initial commit

---
+++ docs/js/gcpe.js
@@ -0,0 +1,130 @@
+var jsonDBs = [
+  {"url": "https://jsonbox.io/sPIRITsGroupTastingForGin/" },  // https://github.com/vasanthv/jsonbox#readme
+  {"url": "https://jsonbase.com/sPIRITsGroupTastingForGin/" }
+
+// https://jsonstorage.net/
+
+];
+
+function getGinDb() {
+ return jsonDBs[0].url;
+}
+
+var corsProxies = [
+  {"url": "https://cors-anywhere.herokuapp.com/", "p": 0.4, "content": null},      // 200 in 60 min 
+  {"url": "https://api.allorigins.win/get?url=", "p": 0.6, "content": "contents"}  // goes into contents:
+ // NOT WORKING
+ // "https://cors-proxy.htmldriven.com/?url=", // 404
+ // "https://thingproxy.freeboard.io/fetch/", // ??
+ // "http://www.whateverorigin.org/get?url=", // https needed
+ // "http://alloworigin.com/get?url=", // https needed
+ // "https://yacdn.org/proxy/"  // <uri>?maxAge=10  bad gateway
+];
+
+// geo:  https://stackoverflow.com/questions/391979/how-to-get-clients-ip-address-using-javascript
+
+function getRandomProxy() {
+  var randomFloat = Math.random();
+  var proxy = null
+  for (var i = 0; i < corsProxies.length; i++ ) {  
+    proxy = corsProxies[i];
+    randomFloat -= proxy.p;
+    if(randomFloat < 0.0) {
+      return proxy.url;
+    }
+  }
+  return proxy.url;
+}
+
+var makeCRCTable = function(){
+    var c;
+    var crcTable = [];
+    for(var n =0; n < 256; n++){
+        c = n;
+        for(var k =0; k < 8; k++){
+            c = ((c&1) ? (0xEDB88320 ^ (c >>> 1)) : (c >>> 1));
+        }
+        crcTable[n] = c;
+    }
+    return crcTable;
+}
+
+var crc32 = function(str) {
+    var crcTable = window.crcTable || (window.crcTable = makeCRCTable());
+    var crc = 0 ^ (-1);
+
+    for (var i = 0; i < str.length; i++ ) {
+        crc = (crc >>> 8) ^ crcTable[(crc ^ str.charCodeAt(i)) & 0xFF];
+    }
+
+    return (crc ^ (-1)) >>> 0;
+};
+
+function getFingerprint(days=5.0, delta=0.0) {
+  var navigator_info = window.navigator;
+  var screen_info = window.screen;
+  var uid = navigator_info.mimeTypes.length;
+  var dateTs = Date.now() - 1000*60*60*24*delta;           // i.e. 2 days
+  var dateTs = Math.round(dateTs/(1000*60*60*24*days));    // i.e. 4 days
+  uid += navigator_info.userAgent.replace(/\D+/g, '');
+  uid += navigator_info.plugins.length;                   // plugins: name,version
+  uid += screen_info.height || '';
+  uid += screen_info.width || '';
+  uid += screen_info.pixelDepth || '';
+  uid += dateTs.toString();
+  return crc32(uid);
+}
+
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    var items = location.search.substr(1).split("&");
+    for (var index = 0; index < items.length; index++) {
+        tmp = items[index].split("=");
+        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+    }
+    return result;
+}
+
+function checkUrlSubString(str) {
+  return (location.href.indexOf(str) != -1);
+}
+
+function isoStr(str) {
+   if (!str) return '';
+   str = str.replace(' ','_');
+   str = str.replace('ß','ss');
+   str = str.replace('ü','ue');
+   return str.toLowerCase();
+}
+
+
+function transparentize(color, opacity) {
+  var alpha = opacity === undefined ? 0.5 : 1 - opacity;
+  return Color(color).alpha(alpha).rgbString();
+}
+
+
+var vueGCPE = new Vue({
+  el: '#gcpe', 
+  data: {
+        uid: '0',
+        uidOld: '0',
+  },
+  methods: { 
+
+  },
+  computed: {
+
+  },
+  filters: {
+    lowercase: function (str) {
+      return isoStr(str);
+    }
+  },
+  mounted () { 
+     this.initTs = Date.now();
+     this.uid = getFingerprint(4.0, 0.0);
+     this.uidOld = getFingerprint(4.0, 2.0);
+  }
+}) 
