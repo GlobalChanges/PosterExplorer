98e5454948ff2df9173d911d94a5f8f9ad6e91a5
Michael Kahle <michael.kahle@yahoo.de>
2022-03-13 19:54:53 +0100

    try focus

--- docs/js/gcpe.js
+++ docs/js/gcpe.js
@@ -220,6 +220,7 @@ var vueGCPE = new Vue({
      toggleModal() {
       const body = document.querySelector("body");
       this.active = !this.active;
+      //this.$refs.modalClose.$el.focus();
       this.active
         ? body.classList.add("modal-open")
         : body.classList.remove("modal-open");
@@ -876,7 +877,6 @@ var vueGCPE = new Vue({
      //this.setPage('gallery');
      this.checkMap();
      this.initSeadragon();
-     this.$refs.modalClose.$el.focus();
   },
   created () {
      this.inqLocale('de');
