6f240ebb021cd985fc479c757e930edf85106881
KMicha <michael.kahle@yahoo.de>
2022-06-07 16:47:53 +0200

    inq from freidok

--- docs/js/freidok.js
+++ docs/js/freidok.js
@@ -6,6 +6,7 @@ var vueFreidok = new Vue({
          allPosterData: [],
          allDirectoriesData: [],
          allCountries : [], 
+         allTitles : [],
         },
   methods: {  
      inqCountries: function () {
@@ -17,13 +18,42 @@ var vueFreidok = new Vue({
        });
      },
      setCountries: function(data) { this.allCountries = data; },
+     inqFreidok: function (freidokId) {
+       var freidokUrl = "https://freidok.uni-freiburg.de/jsonApi/v1/publications?available=issued&fieldset=lp&publicationId="+freidokId;
+       axios
+         .get(freidokUrl)
+         .then(response => { 
+            this.handleFreidok(response.data);
+       });
+     },
+     handleFreidok: function(data) {
+       if(data.numFound == 1) {
+          var freidok = data.docs[0];
+          if((freidok.pubtype.type == 'collection') || (freidok.pubtype.type == 'hierarchical_object'))  {
+             for(var j=0; j<freidok.reverse_relations.length; j++) {
+               var reverseDok = freidok.reverse_relations[j];
+               if(reverseDok.type == 'is_part_of_rev') {
+                 var nextId = reverseDok.norm_id.toString();
+                 this.inqFreidok(nextId); 
+               }        
+             }
+          }
+          if(freidok.pubtype.type == 'poster') {
 
-
+          }
+          var title = "???";
+          for(var t=0; t<freidok.titles.length; t++) {
+             title = freidok.titles[t].value; 
+          }
+          this.allTitles.push({id:freidok.id.toString(), title:title, type:freidok.pubtype.type});
+      } 
+    }
 
   },
  mounted () { 
      // this.inqFolders();
-
+     var startID = 165768;
+     this.inqFreidokById(startID);
   },
   created () {
      this.inqCountries();
