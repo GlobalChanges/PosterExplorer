ddbcf232da00a88212dd380ff5d05b18c086b078
KMicha <michael.kahle@yahoo.de>
2024-04-23 13:54:38 +0200

    add new folders and iran

--- docs/js/map.js
+++ docs/js/map.js
@@ -89,7 +89,10 @@ function getMarkerOptions(property) {
   var color = 'violet';
   var html = "<br style='font-size: 50%'/><b>"+property.id+"</b>";
   if(property.icon) {
-     color = 'orange-dark';
+     color = 'blue-dark';
+     if(property.freidok) {
+       color = 'orange-dark';
+     }
      html = "<img src='"+property.icon+"' width='22' height='22' class='marker-image' ></img>";
   } 
   var extraOptions = {icon: 'fa-number',
